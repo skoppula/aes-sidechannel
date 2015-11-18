7474af0b902b3e36deae6d5f463598d0fe2e83a1
utsav999 <utsav@mit.edu>
2015-11-18 18:30:52 -0500

    added arduino and organized

--- arduino-code/aes-sidechannel.ino
+++ arduino-code/aes-sidechannel.ino
@@ -3,20 +3,38 @@
 
 #include <AESLib.h>
 
+char data[17];    // Allocate some space for the data string
+char inChar = -1; // Where to store the character read
+byte index = 0;   // Index into array; where to store the character
+uint8_t key[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}; //16 bytes of Key
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(13, OUTPUT);
   digitalWrite(13,LOW);
-  delay(1000);
-  uint8_t key[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}; //16 bytes
-  char data[] = "ABCDEF1234567890"; //16 bytes
-  digitalWrite(13,HIGH);
-  aes128_enc_single(key, data);
-  digitalWrite(13,LOW);
-  Serial.println(data);
+  data[16]='\0';
 }
 
 void loop() {
+  while (Serial.available() > 0) // Don't read unless you know there is data
+  {
+    if(index < 16) {
+      inChar = Serial.read(); // Read a character
+      data[index] = inChar;   // Store it
+      index++;                // Increment where to write next
+    } else {
+      digitalWrite(13,HIGH);
+      Serial.println("Start");
+      Serial.println(data);
+      aes128_enc_single(key, data);
+      digitalWrite(13,LOW);
+      Serial.println(data);
+      Serial.println("DONE");
+      aes128_dec_single(key,data);
+      Serial.println(data);
+      index = 0;       
+    }
+  }
 
 }
