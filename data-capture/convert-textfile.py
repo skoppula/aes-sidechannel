319aa3b80ea277495de993018dcfa7fca69aec20
Skanda Koppula <skanda.koppula@gmail.com>
2015-11-23 22:52:59 -0500

    new plaintext nums

--- data-capture/convert-textfile.py
+++ data-capture/convert-textfile.py
@@ -2,19 +2,19 @@
 
 import random
 
-# with open('./data/plaintexts.txt', 'r') as f:
-#     plaintext_bytes = []
-#     for line in [l.strip() for l in f.readlines()]:
-#         plaintext_bytes.append([int(s, 16) for s in line.split(" ")])
+with open('./data/plaintexts.txt', 'r') as f:
+    plaintext_bytes = []
+    for line in [l.strip() for l in f.readlines()]:
+        plaintext_bytes.append([int(s, 16) for s in line.split(" ")])
 
-# size_of_line = len(plaintext_bytes[0])
+size_of_line = len(plaintext_bytes[0])
 
-size_of_line = 16
-plaintext_bytes = []
+# size_of_line = 16
+# plaintext_bytes = []
 
-char_codes = range(33,127)
+char_codes = range(0,256)
 
-extra_plaintexts_count = 500
+extra_plaintexts_count = 300
 for i in range(extra_plaintexts_count):
         plaintext_bytes.append([char_codes[random.randint(0,len(char_codes)-1)] for s in range(size_of_line)])
 
