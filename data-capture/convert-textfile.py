9c468feede86b6fe8278156397418b9727d8a395
Skanda Koppula <skanda.koppula@gmail.com>
2015-11-23 22:14:28 -0500

    limit rand chars to 33 to 136

--- data-capture/convert-textfile.py
+++ data-capture/convert-textfile.py
@@ -2,16 +2,21 @@
 
 import random
 
-with open('./data/plaintexts.txt', 'r') as f:
-    plaintext_bytes = []
-    for line in [l.strip() for l in f.readlines()]:
-        plaintext_bytes.append([int(s, 16) for s in line.split(" ")])
+# with open('./data/plaintexts.txt', 'r') as f:
+#     plaintext_bytes = []
+#     for line in [l.strip() for l in f.readlines()]:
+#         plaintext_bytes.append([int(s, 16) for s in line.split(" ")])
 
-size_of_line = len(plaintext_bytes[0])
+# size_of_line = len(plaintext_bytes[0])
 
-extra_plaintexts_count = 300
+size_of_line = 16
+plaintext_bytes = []
+
+char_codes = range(33,127)
+
+extra_plaintexts_count = 500
 for i in range(extra_plaintexts_count):
-        plaintext_bytes.append([random.randint(0,255) for s in range(size_of_line)])
+        plaintext_bytes.append([char_codes[random.randint(0,len(char_codes)-1)] for s in range(size_of_line)])
 
 num_lines = len(plaintext_bytes)
 assert size_of_line  == 16
