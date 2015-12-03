802b17d41a0b5da77e1013e1111f4643bc19b8d6
Skanda Koppula <skanda.koppula@gmail.com>
2015-12-02 22:44:24 -0500

    plaintexts

--- cpa/myload.py
+++ cpa/myload.py
@@ -8,35 +8,13 @@ import wfm2read_fast
 def loadFromNPFile(fname):
     return np.load(fname)
 
-def filter_plaintexts(lst):
-    with open('./hex_plaintexts.txt', 'r') as f:
-        lines = f.readlines()
-    for i, line in enumerate(lines):
-        if (i+1) not in lst:
-            lines[i] = None
-    new_lines = filter(lambda x: x != None, lines)
-    with open('./hex_plaintexts_288_first159.txt', 'w') as f:
-        lines = f.writelines(new_lines)
-        
-
 def readWFMs(infolder, outfile):
     print 'Reading WFMs from', infolder
 
-    start_frac, end_frac = (0.1,0.8)
-
     all_files =  os.listdir(infolder)
-    num_traces = 159
-    files_included = []
-    for file_count in xrange(1,num_traces):
-        for fyle in all_files:
-            if 'W' + str(file_count) + '-'  in fyle:
-                break
-        if fyle.split('-')[1].startswith('288'):
-            files_included.append(file_count)
-    filter_plaintexts(files_included)
 
     z = 0
-    for file_count in xrange(1,num_traces):
+    for file_count in xrange(-2,len(all_files)-2):
         for fyle in all_files:
             if 'W' + str(file_count) + '-'  in fyle:
                 break
@@ -45,15 +23,15 @@ def readWFMs(infolder, outfile):
         print '\tReading ', infolder+fyle, ' ', file_count,'/', num_traces
         trace_values = wfm2read_fast.wfm2read(infolder+fyle)[0] 
         trimmed_values = trace_values[len(trace_values)*start_frac:len(trace_values)*end_frac]
-        def avg_map(arr, numSamps):
-            avged_arr = []
-            for i in range(len(arr)/numSamps):
-                nextClump = arr[i*numSamps:i*numSamps+numSamps]
-                nextValue = sum(nextClump)/numSamps
-                avged_arr.append(nextValue)
-            return avged_arr
-
-        averaged_values = avg_map(trimmed_values, 1)
+#         def avg_map(arr, numSamps):
+#             avged_arr = []
+#             for i in range(len(arr)/numSamps):
+#                 nextClump = arr[i*numSamps:i*numSamps+numSamps]
+#                 nextValue = sum(nextClump)/numSamps
+#                 avged_arr.append(nextValue)
+#             return avged_arr
+# 
+        # averaged_values = avg_map(trimmed_values, 1)
         try:
             traces
         except NameError:
@@ -65,5 +43,5 @@ def readWFMs(infolder, outfile):
     print 'Finished processing all WFMs. Output in',outfile
 
 if '__main__' == __name__:
-    readWFMs(infolder='/media/usb/11-30-2015-500-attempt5/',outfile='11-30-2015-attempt5-only288-first159')
+    readWFMs(infolder='../../trace-data/12-02-2015-100/',outfile='12-02-2015-100plaintexts-XOR')
 
