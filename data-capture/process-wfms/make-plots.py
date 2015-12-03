852d9b4d7cd19e147e76642d3905523e3250b41d
Skanda Koppula <skanda.koppula@gmail.com>
2015-12-02 21:13:19 -0500

    finish

--- data-capture/process-wfms/make-plots.py
+++ data-capture/process-wfms/make-plots.py
@@ -2,15 +2,15 @@ import gen_plot as gp
 import matplotlib.pyplot as plt
 
 filenames = [
-        'W375-601.wfm',
-        #'W371-288.wfm',
-        'W377-641.wfm',
-        #'W331-288.wfm',
+        'W-2-99.wfm',
+        'W-1-3.wfm',
+        'W0-3.wfm',
+        'W97-3.wfm',
         ]
 
 plt.gca().set_color_cycle(['red', 'green', 'blue', 'yellow', 'purple'])
 
 for fyle in filenames:
-    plt.plot(*gp.plot('/media/usb/11-30-2015-500-attempt5/' + fyle))
+    plt.plot(*gp.plot('/home/skoppula/mit/security/final-project/trace-data/12-02-2015-100/' + fyle))
 
 plt.show()
