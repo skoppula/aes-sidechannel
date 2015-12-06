5011c9a735f07368817bfa9960f75421b4b89951
Lisa Ho <lisa71548@gmail.com>
2015-12-05 23:29:29 -0500

    sending rs to the arduino

--- data-capture/arduino-aes/pyth.py
+++ data-capture/arduino-aes/pyth.py
@@ -3,10 +3,10 @@ import time
 ser = serial.Serial('/dev/cu.usbmodem1411')
 print('Port in use is ' + ser.name)
 
-numTraces = 500
+num_traces = 500
 
-for i in range(0, numTraces):
-    ser.write(b"R")
+for i in range(0, num_traces):
+    ser.write(b"r")
     time.sleep(1)
 
 ser.close()
